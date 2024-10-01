786dffea24199b562051d085b69d13420d074945
emiruuzun <emirsah.41@gmail.com>
2024-10-01 16:01:11 +0300

    fix apexchart

--- src/pages/admin/PersonelRegister/index.jsx
+++ src/pages/admin/PersonelRegister/index.jsx
@@ -1,183 +1,200 @@
 import React, { useState } from "react";
 import AdminDashboardlayout from "../../../layout/AdminDashboard";
-import { registerUser } from "../../../services/admin"; // Servis fonksiyonunu import et
+import { registerUser } from "../../../services/admin";
+import {
+  FaUser,
+  FaEnvelope,
+  FaLock,
+  FaIdCard,
+  FaBriefcase,
+  FaPhone,
+  FaUsers,
+  FaToggleOn,
+} from "react-icons/fa";
+import { toast } from "react-toastify";
 
 function PersonelRegister() {
-  // Her alan için ayrı ayrı state tanımlamaları
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [tcNo, setTcNo] = useState("");
-  const [position, setPosition] = useState("");
-  const [contact, setContact] = useState("");
-  const [status, setStatus] = useState("Aktif"); // Varsayılan olarak 'Aktif' ayarladık
-  const [group, setGroup] = useState(""); // Group için state
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    tcNo: "",
+    position: "",
+    contact: "",
+    status: "Aktif",
+    group: "",
+  });
 
-  // Form gönderim fonksiyonu
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Sayfanın yeniden yüklenmesini engelle
-    const user = {
-      name,
-      email,
-      password,
-      tcNo,
-      position,
-      contact,
-      status,
-      group, // Group'u ekledik
-    };
-
+    e.preventDefault();
     try {
-      const response = await registerUser(user); // registerUser servis fonksiyonunu çağır
+      const response = await registerUser(formData);
       if (response.success) {
-        setName("");
-        setEmail("");
-        setPassword("");
-        setTcNo("");
-        setPosition("");
-        setContact("");
-        setStatus("Aktif"); // Varsayılan olarak 'Aktif' ayarladık
-        setGroup(""); // Group alanını sıfırla
+        toast.success("Kullanıcı başarıyla kaydedildi!");
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+          tcNo: "",
+          position: "",
+          contact: "",
+          status: "Aktif",
+          group: "",
+        });
       }
     } catch (error) {
       console.error("Kayıt hatası:", error);
+      toast.error("Kayıt sırasında bir hata oluştu.");
     }
   };
 
   return (
     <AdminDashboardlayout>
-      <div className="p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-4 text-gray-800">
-          Kullanıcı Kaydet
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              İsim
-            </label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
+      <div className="p-6 bg-gradient-to-r from-blue-100 to-indigo-100 min-h-screen">
+        <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-xl overflow-hidden">
+          <div className="px-6 py-4 bg-indigo-600 text-white text-center">
+            <h2 className="text-3xl font-bold">Yeni Personel Kaydı</h2>
           </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Şifre
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              TC No
-            </label>
-            <input
-              type="text"
-              name="tcNo"
-              value={tcNo}
-              onChange={(e) => setTcNo(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              pattern="\d{11}" // Sadece 11 basamaklı sayılar için
-              maxLength="11"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Pozisyon
-            </label>
-            <input
-              type="text"
-              name="position"
-              value={position}
-              onChange={(e) => setPosition(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              İletişim
-            </label>
-            <input
-              type="tel"
-              name="contact"
-              value={contact}
-              onChange={(e) => setContact(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              pattern="\d+" // Sadece sayılar için
-              maxLength={11}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Grup
-            </label>
-            <select
-              name="group"
-              value={group}
-              onChange={(e) => setGroup(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            >
-              <option value="" disabled>
-                Grup Seçin
-              </option>
-              <option value="Mekanik">Mekanik</option>
-              <option value="Boru">Boru</option>
-              <option value="Elektrik">Elektrik</option>
-              <option value="Aksaray">Aksaray</option>
-              <option value="Kapı">Kapı</option>
-              <option value="Ofis">Ofis</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Durum
-            </label>
-            <select
-              name="status"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-            >
-              <option value="Aktif">Aktif</option>
-              <option value="İzinli">İzinli</option>
-              <option value="Pasif">Pasif</option>
-            </select>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-blue-600 text-white font-bold rounded-lg"
-          >
-            Kaydet
-          </button>
-        </form>
+          <form onSubmit={handleSubmit} className="p-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaUser className="mr-2" /> İsim
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaEnvelope className="mr-2" /> Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaLock className="mr-2" /> Şifre
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaIdCard className="mr-2" /> TC No
+                </label>
+                <input
+                  type="text"
+                  name="tcNo"
+                  value={formData.tcNo}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  pattern="\d{11}"
+                  maxLength="11"
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaBriefcase className="mr-2" /> Pozisyon
+                </label>
+                <input
+                  type="text"
+                  name="position"
+                  value={formData.position}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaPhone className="mr-2" /> İletişim
+                </label>
+                <input
+                  type="tel"
+                  name="contact"
+                  value={formData.contact}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  pattern="\d+"
+                  maxLength={11}
+                  required
+                />
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaUsers className="mr-2" /> Grup
+                </label>
+                <select
+                  name="group"
+                  value={formData.group}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  required
+                >
+                  <option value="" disabled>
+                    Grup Seçin
+                  </option>
+                  <option value="Mekanik">Mekanik</option>
+                  <option value="Boru">Boru</option>
+                  <option value="Elektrik">Elektrik</option>
+                  <option value="Aksaray">Aksaray</option>
+                  <option value="Kapı">Kapı</option>
+                  <option value="Ofis">Ofis</option>
+                </select>
+              </div>
+              <div className="relative">
+                <label className="text-gray-700 font-bold mb-2 flex items-center">
+                  <FaToggleOn className="mr-2" /> Durum
+                </label>
+                <select
+                  name="status"
+                  value={formData.status}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                  <option value="Aktif">Aktif</option>
+                  <option value="İzinli">İzinli</option>
+                  <option value="Pasif">Pasif</option>
+                </select>
+              </div>
+            </div>
+            <div className="mt-8">
+              <button
+                type="submit"
+                className="w-full py-3 px-4 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+              >
+                Personel Kaydet
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </AdminDashboardlayout>
   );
